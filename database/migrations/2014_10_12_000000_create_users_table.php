a06b525302b6562199e870b157880f4d316b9916
y-nagaharat <y.nagahara@lassic.co.jp>
2020-05-21 02:01:14 +0000

    user registration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('is_deleted')->default("False");
             $table->rememberToken();
             $table->timestamps();
         });
